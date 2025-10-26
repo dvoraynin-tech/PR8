248380fcb465e38fb7c02f7e7ffbc1d6c6b41fb1
dvoraynin-tech <dvoraynin@gmail.com>
2025-10-26 15:15:47 +0200

    Запис першого питання
    Виконав Бобошко Вадим

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Створення текстового файлу з обробкою виключень - Бобошко Вадим
 def create_file():
     try:
-        # спробуємо створити файл для запису
         file = open("students_questions.txt", "w", encoding="utf-8")
         file.write("Файл для питань і відповідей з Python\n")
         file.close()
@@ -11,3 +10,17 @@ def create_file():
         print("Помилка: немає дозволу на запис у файл.")
 
 create_file()
+
+# Перше питання - Бобошко Вадим
+def first_question():
+    try:
+        file = open("students_questions.txt", "a", encoding="utf-8")
+        file.write("Питання Бобошка Вадима\n")
+        file.write("1. Що таке список (list) у Python і як можна додати елемент у список?\n")
+        file.close()
+    except FileNotFoundError:
+        print("Помилка: файл не знайдено.")
+    except PermissionError:
+        print("Помилка: немає дозволу на запис у файл.")
+
+first_question()
